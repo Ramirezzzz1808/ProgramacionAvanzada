86d12122e28e972fe9a38edc3d2d6fb218e727ad
Ramirezzzz1808 <picoantonio1808@gmail.com>
2025-04-06 22:18:18 -0500

    Add files via upload

---
+++ TiposDeDatos/TiposDeDatos/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TiposDeDatos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+
+            int contador = 0;
+
+
+            
+            do
+
+            {
+
+                Console.WriteLine($"Contador: {contador}");
+
+                contador++;
+
+            } while (contador < 3);
+
+
+
+            Console.ReadKey();
+
+        }
+
+    }
+}
