05dbdd40d1f4e34cc11aedf648ee9aefba1444be
Ramirezzzz1808 <picoantonio1808@gmail.com>
2025-04-06 22:15:57 -0500

    Add files via upload

---
+++ Enumeracion2Framewokr/Enumeracion2Framewokr/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace Enumeracion2Framewokr
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmbEstado = new System.Windows.Forms.ComboBox();
+            this.btnMostrar = new System.Windows.Forms.Button();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cmbEstado
+            // 
+            this.cmbEstado.FormattingEnabled = true;
+            this.cmbEstado.Location = new System.Drawing.Point(57, 88);
+            this.cmbEstado.Name = "cmbEstado";
+            this.cmbEstado.Size = new System.Drawing.Size(181, 24);
+            this.cmbEstado.TabIndex = 0;
+            // 
+            // btnMostrar
+            // 
+            this.btnMostrar.Location = new System.Drawing.Point(339, 88);
+            this.btnMostrar.Name = "btnMostrar";
+            this.btnMostrar.Size = new System.Drawing.Size(160, 38);
+            this.btnMostrar.TabIndex = 1;
+            this.btnMostrar.Text = "button1";
+            this.btnMostrar.UseVisualStyleBackColor = true;
+            this.btnMostrar.Click += new System.EventHandler(this.btnMostrar_Click);
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Location = new System.Drawing.Point(79, 57);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(69, 16);
+            this.lblResultado.TabIndex = 2;
+            this.lblResultado.Text = "Resultado";
+            this.lblResultado.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.btnMostrar);
+            this.Controls.Add(this.cmbEstado);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbEstado;
+        private System.Windows.Forms.Button btnMostrar;
+        private System.Windows.Forms.Label lblResultado;
+    }
+}
+
---
+++ Enumeracion2Framewokr/Enumeracion2Framewokr/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Enumeracion2Framewokr
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            // Cargar enum en ComboBox 
+            cmbEstado.DataSource = Enum.GetValues(typeof(EstadoPedido));
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnMostrar_Click(object sender, EventArgs e)
+        {
+            // Obtener valor seleccionado del ComboBox 
+            EstadoPedido estadoSeleccionado = (EstadoPedido)cmbEstado.SelectedItem;
+
+
+            switch (estadoSeleccionado)
+            {
+                case EstadoPedido.Pendiente:
+                    lblResultado.Text = "Tu pedido está pendiente.";
+                    break;
+                case EstadoPedido.Procesando:
+                    lblResultado.Text = "Tu pedido está siendo procesado.";
+                    break;
+                case EstadoPedido.Enviado:
+                    lblResultado.Text = "Tu pedido ha sido enviado.";
+                    break;
+                case EstadoPedido.Entregado:
+                    lblResultado.Text = "Tu pedido fue entregado.";
+                    break;
+                case EstadoPedido.Cancelado:
+                    lblResultado.Text = "Tu pedido fue cancelado.";
+                    break;
+                default:
+                    lblResultado.Text = "Estado desconocido.";
+                    break;
+            }
+        }
+    }
+} 
+        
