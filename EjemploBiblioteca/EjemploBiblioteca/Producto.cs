c4942e96b380d670a232609e48a5582de49637a7
Ramirezzzz1808 <picoantonio1808@gmail.com>
2025-04-21 10:10:55 -0500

    Add files via upload

---
+++ EjemploBiblioteca/EjemploBiblioteca/Producto.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjemploBiblioteca
+{
+    public class Producto
+
+    {
+
+        // Campos privados 
+
+        private string nombre;
+
+        private decimal precio;
+
+
+
+        // Propiedades públicas 
+
+        public string Nombre
+
+        {
+
+            get { return nombre; }
+
+            set { nombre = value; }
+
+        }
+
+
+
+        public decimal Precio
+
+        {
+
+            get { return precio; }
+
+            set { precio = value; }
+
+        }
+
+
+
+        // Método que retorna descripción 
+
+        public string ObtenerDescripcion()
+
+        {
+
+            return $"Producto: {Nombre}, Precio: {Precio:C}";
+
+        }
+
+    }
+
+}
